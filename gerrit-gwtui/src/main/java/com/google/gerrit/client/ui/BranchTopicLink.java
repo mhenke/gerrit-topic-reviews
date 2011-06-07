9a36841c566441f6e2b69ccbb30d13a1fecc0fbe
Diego Celix <diego.celix@codethink.co.uk>
2011-06-07 09:49:37 +0100

    BranchTopicLink: Widget handler for Topic&Branch links

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/BranchTopicLink.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.ui;
+
+import com.google.gerrit.reviewdb.Change;
+import com.google.gerrit.reviewdb.Change.Id;
+import com.google.gerrit.reviewdb.Project;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+/** Link to the open changes of a project. */
+public class BranchTopicLink extends Composite {
+  private final HorizontalPanel hp;
+  private final BranchLink bl;
+  private final TopicLink tl;
+
+  public BranchTopicLink(Project.NameKey project, Change.Status status,
+      String branch, String topic, Id topicId) {
+    this(branch, project, status, branch, topic, topicId);
+  }
+
+  public BranchTopicLink(String text, Project.NameKey project, Change.Status status,
+      String branch, String topic, Id topicId) {
+    hp = new HorizontalPanel();
+
+    bl = new BranchLink(text, project, status, branch, topic);
+    if (topicId != null) tl = new TopicLink(" (" + topic + ")", topicId);
+    else tl = null;
+
+    hp.add(bl);
+    hp.add(tl);
+    initWidget(hp);
+  }
+}

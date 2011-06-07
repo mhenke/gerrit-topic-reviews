34942c908a886791325d52d74029d30cad66b7dc
Diego Celix <diego.celix@codethink.co.uk>
2011-06-07 09:47:28 +0100

    TopicLink: Added new link type

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/TopicLink.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.ui;
+
+import com.google.gerrit.client.Gerrit;
+import com.google.gerrit.client.changes.TopicScreen;
+import com.google.gerrit.common.PageLinks;
+import com.google.gerrit.reviewdb.Change.Id;
+
+/** Link to the open changes of a topic. */
+public class TopicLink extends InlineHyperlink {
+  private final Id topicId;
+
+  public TopicLink(String topic, Id topicId) {
+    super(topic, PageLinks.toTopic(topicId));
+    this.topicId = topicId;
+  }
+
+  @Override
+  public void go() {
+    Gerrit.display(getTargetHistoryToken(), createScreen());
+  }
+
+  private Screen createScreen() {
+    return new TopicScreen(topicId);
+  }
+}
